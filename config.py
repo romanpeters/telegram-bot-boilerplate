3d20cf87712e0d2e0b9dc9932c848b15e22444d9
Roman Peters <github@romanpeters.nl>
2020-02-25 13:13:32 +0100

    Add quick start documentation

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-import redacted
+try:
+    import redacted
+except ImportError:
+    print("No redacted.py found. Create your own, or add your values to config.py")
 import logging
 
 BOT_TOKEN = redacted.BOT_TOKEN
-ADMINS = ["@RomanPeters"]
+ADMINS = []
 WHITELIST_CHATS = []
 LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_LEVEL = logging.INFO
